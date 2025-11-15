a097d62b0f09948aabc576b165ad618e0e5c5a1a
Araz Alkhasov <arazalkhasov@gmail.com>
2025-11-15 20:24:47 +0400

    Update UIButtonHandler.cs

--- UIButtonHandler.cs
+++ UIButtonHandler.cs
@@ -3,24 +3,25 @@ using UnityEngine.UI;
 
 public class UIButtonHandler : MonoBehaviour
 {
-    public MistralChatBot chatBot;  // Ссылка на MistralChatBot
-    public Button sendButton;  // Ссылка на кнопку отправки
-    public InputField userInput;  // Ссылка на поле ввода
+    public MistralChatBot chatBot;  
+    public Button sendButton;  
+    public InputField userInput;  
    
 
     void Start()
     {
-        sendButton.onClick.AddListener(OnSendButtonClick);  // Привязка метода к кнопке
+        sendButton.onClick.AddListener(OnSendButtonClick);  
     }
 
-    // Метод, который срабатывает при нажатии на кнопку
+   
     public void OnSendButtonClick()
     {
-        string userMessage = userInput.text;  // Получаем текст из поля ввода
+        string userMessage = userInput.text;  
         if (!string.IsNullOrEmpty(userMessage))
         {
-            StartCoroutine(chatBot.GetChatResponse(userMessage));  // Отправляем запрос
+            StartCoroutine(chatBot.GetChatResponse(userMessage)); 
         }
     }
 }
+
 
